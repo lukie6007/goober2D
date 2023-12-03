abbb634f1fd55760e77804f45a03f009bcf7aacb
Lukas <92491084+lukie6007@users.noreply.github.com>
2023-12-03 17:18:12 +0000

    merge conflicts again everything is broken

--- index.js
+++ index.js
@@ -1,48 +1,46 @@
-const cssDropdown = `<select id="cssProperties" onchange="applyProperty()">
+const cssDropdown = `
+  <select id="cssProperties" onchange="applyProperty()">
     <option selected disabled value="">Select a property</option>
 
     <optgroup label="Text Properties">
-        <option value="color">Color</option>
-        <option value="font-size">Font Size</option>
-        <option value="font-family">Font Family</option>
-        <option value="text-align">Text Align</option>
-        <option value="text-decoration">Text Decoration</option>
-        <option value="line-height">Line Height</option>
+      <option value="color">Color</option>
+      <option value="font-size">Font Size</option>
+      <option value="font-family">Font Family</option>
+      <option value="text-align">Text Align</option>
+      <option value="text-decoration">Text Decoration</option>
+      <option value="line-height">Line Height</option>
     </optgroup>
 
     <optgroup label="Box Model Properties">
-        <option value="margin">Margin</option>
-        <option value="padding">Padding</option>
-        <option value="border">Border</option>
-        <option value="width">Width</option>
-        <option value="height">Height</option>
-        <option value="display">Display</option>
-        <option value="position">Position</option>
-        <option value="top">Top</option>
-        <option value="right">Right</option>
-        <option value="bottom">Bottom</option>
-        <option value="left">Left</option>
-        <option value="float">Float</option>
-        <option value="clear">Clear</option>
+      <option value="margin">Margin</option>
+      <option value="padding">Padding</option>
+      <option value="border">Border</option>
+      <option value="width">Width</option>
+      <option value="height">Height</option>
+      <option value="display">Display</option>
+      <option value="position">Position</option>
+      <option value="top">Top</option>
+      <option value="right">Right</option>
+      <option value="bottom">Bottom</option>
+      <option value="left">Left</option>
+      <option value="float">Float</option>
+      <option value="clear">Clear</option>
     </optgroup>
 
     <optgroup label="Background Properties">
-        <option value="background-color">Background Color</option>
-        <option value="background-image">Background Image</option>
-        <option value="background-size">Background Size</option>
-        <option value="background-position">Background Position</option>
+      <option value="background-color">Background Color</option>
+      <option value="background-image">Background Image</option>
+      <option value="background-size">Background Size</option>
+      <option value="background-position">Background Position</option>
     </optgroup>
 
     <optgroup label="Flexbox Properties">
-        <option value="flex">Flex</option>
-        <option value="flex-direction">Flex Direction</option>
-        <option value="justify-content">Justify Content</option>
-        <option value="align-items">Align Items</option>
+      <option value="flex">Flex</option>
+      <option value="flex-direction">Flex Direction</option>
+      <option value="justify-content">Justify Content</option>
+      <option value="align-items">Align Items</option>
     </optgroup>
-
-    <!-- Add more groups and options as needed -->
-</select>`
-
+  </select>`;
 
 let elementPropertiesInfo = {
     "div": [
@@ -118,8 +116,7 @@ let elementPropertiesInfo = {
         "BASIC",
         { "label": "Visual Properties", "type": "heading" },
         { "label": "Style", "property": "style", "type": "script", "id": "inputStyle" },
-        { "label": "Placeholder", "property": "innerHTML", "type": "text", "id": "inputPlaceholder" }
-    ],
+        { "label": "Placeholder", "property": "innerHTML", "type": "text", "id": "inputPlaceholder" }]
 };
 
 function replaceKeywordsWithSet(obj, keyword, replacement) {
@@ -127,7 +124,6 @@ function replaceKeywordsWithSet(obj, keyword, replacement) {
         if (obj.hasOwnProperty(key) && Array.isArray(obj[key])) {
             const index = obj[key].indexOf(keyword);
             if (index !== -1) {
-                // Replace the keyword with the new set of properties
                 obj[key].splice(index, 1, ...replacement);
             }
         }
@@ -149,30 +145,23 @@ let visualProperties = [
 replaceKeywordsWithSet(elementPropertiesInfo, "BASIC", basicProperties);
 replaceKeywordsWithSet(elementPropertiesInfo, "VISUAL", visualProperties);
 
-let page = {
-    "elements": []
-};
+let page = { "elements": [] };
 
 function loadPage() {
     let preview = document.getElementById('pageload');
-
     if (preview) {
         preview.innerHTML = '';
-
         for (let i = 0; i < page.elements.length; i++) {
             let elementInfo = page.elements[i];
             let newElement = document.createElement(elementInfo.elementType);
             newElement.GENERATED = true;
             newElement.pageID = i;
-
-            // Set properties
             for (let property in elementInfo.properties) {
                 newElement[property] = elementInfo.properties[property];
             }
-
             preview.appendChild(newElement);
         }
-        updateStylePanel()
+        updateStylePanel();
     }
 }
 
@@ -184,8 +173,8 @@ function newElement(type, properties) {
 
     page.elements.push(newElement);
 
-    window.ElementSelected = newElement
-    updatePropertiesPanel(newElement)
+    window.ElementSelected = newElement;
+    updatePropertiesPanel(newElement);
     loadPage();
 }
 
@@ -203,13 +192,12 @@ function deleteElement(elementSelect) {
         window.ElementSelected = null;
         loadPage();
         updatePropertiesPanel(null);
-
     }
 }
 
 function saveProperties() {
     let element = window.ElementSelected;
-    if (!element) return; // Add error handling for undefined elements
+    if (!element) return;
 
     let properties = elementPropertiesInfo[element.elementType];
 
@@ -227,12 +215,26 @@ function saveProperties() {
 }
 
 function getDropdownElement() {
-    let dropdown = document.getElementById('elementlist')
+    let dropdown = document.getElementById('elementlist');
     if (dropdown.value === 'null') {
-        return 'div'
+        return 'div';
     } else {
-        return dropdown.value
+        return dropdown.value;
     }
+}
+
+function parseInlineCSS(cssString) {
+    const inlineProperties = {};
+    const propertyRegex = /([^:\s]+)\s*:\s*([^;]+);/g;
+
+    let propertyMatch;
+    while ((propertyMatch = propertyRegex.exec(cssString)) !== null) {
+        const property = propertyMatch[1].trim();
+        const value = propertyMatch[2].trim();
+        inlineProperties[property] = value;
+    }
+
+    return inlineProperties;
 }
 
 function objectToHTML(obj, parentElement) {
@@ -253,12 +255,24 @@ function objectToHTML(obj, parentElement) {
             }
 
             ul.appendChild(li);
-
         }
     }
 }
 
+function addStyleProperty(value) {
+    if (window.ElementSelected) {
+        let keyElement = document.getElementById('cssProperties');
+        if (keyElement) {
+            let key = keyElement.value;
+            let currentStyle = window.ElementSelected.properties.style || '';
 
+            if (!currentStyle.includes(`${key}:`)) {
+                window.ElementSelected.properties.style = `${currentStyle} ${key}: ${value};`;
+                updateStylePanel();
+            }
+        }
+    }
+}
 
 function renderPropertyInput(property, element) {
     let inputElement;
@@ -273,6 +287,17 @@ function renderPropertyInput(property, element) {
     }
 
     return `${inputElement}<div></div>`;
+}
+
+function updateStylePanel() {
+    let panel = document.getElementById('style-window')
+    if (panel && window.ElementSelected) {
+
+        let styles = parseInlineCSS(window.ElementSelected.properties.style)
+        let html = objectToHTML('p', styles)
+
+        panel.innerHTML = html
+    }
 }
 
 function updatePropertiesPanel(elementSelect) {
@@ -293,15 +318,13 @@ function updatePropertiesPanel(elementSelect) {
 
             window.ElementSelected = element;
             content += `
-          <div></div>
-          <hr>
-          <button onclick="saveProperties()">Save</button>
-          <button style="background-color: rgb(215, 3, 3);" onclick="deleteElement()">Delete</button>
-        `;
+        <div></div>
+        <hr>
+        <button onclick="saveProperties()">Save</button>
+        <button style="background-color: rgb(215, 3, 3);" onclick="deleteElement()">Delete</button>
+      `;
         }
-
     }
-
 }
 
 document.addEventListener('click', function (event) {
@@ -313,15 +336,10 @@ document.addEventListener('click', function (event) {
 });
 
 document.addEventListener('keydown', function (event) {
-    // Check if the pressed keys are Ctrl (or Command on Mac) and 'S'
     if ((event.ctrlKey || event.metaKey) && event.key === 's') {
-        // Prevent the default browser behavior
         event.preventDefault();
-
-        // Perform your save action or show a message
-        saveProperties()
+        saveProperties();
     }
 });
 
-loadPage()
-
+loadPage();
